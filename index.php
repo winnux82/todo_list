3b77f04f0e4a6148ed32d118f12c7e95af49b33b
WnX <vandermeulen.christophe@gmail.com>
2022-04-06 14:12:28 +0200

    :bulb: liste complete modify supprimer :bulb:

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 //on initialise la session
 session_start();
 //on initialise tout 
@@ -13,11 +14,8 @@ if (isset($_GET['position'])) {
     $url = '?position=' . $position;
 }
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -26,7 +24,6 @@ if (isset($_GET['position'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
-
 <body>
     <!-- Si la position est nulle, c'est un ajout -->
     <?php if ($position === null) : ?>
@@ -35,46 +32,64 @@ if (isset($_GET['position'])) {
     <?php else : ?>
         <h1>Edition</h1>
     <?php endif ?>
-
     <form action="save.php<?= $url ?>" method="POST">
-    <table>
+        <table>
             <tr>
-                <td>Nom : </td>
+                <td></td>
                 <td><input type="text" name="nom" value="<?= $donnees['nom'] ?? '' ?>"><br></td>
-                <td><input type="submit" value="Ajouter" class="btn btn-primary"></td>
+                <td><input type="submit" value="Ajouter"></td>
             </tr>
         </table>
-
     </form>
-
-
     <?php
 
     //Liste
     echo '<h1>To-do List</h1>';
-    echo '<table class="table">';
+    echo '<table>';
     if (isset($_SESSION['donnees'])) {
         //on crée le foreach pour la liste
 
-        if (empty($_SESSION['donnees'])) echo "Le tableau est vide";
+        if (empty($_SESSION['donnees'])) echo "La liste est vide";
         foreach ($_SESSION['donnees'] as $position => $data) {
-        //on donne la liste des donnes
-        echo '<tr>';
-        echo '<td>' . $position . ' </td> ';
-        echo '<td>' . $data['nom'] . ' </td>';
-        echo '</Tr>';
 
+            //on donne la liste des données
+            echo '<tr>';
+            //echo '<td>' . $position . ' </td> '
+            if ($data['check'] == 0) {
+                echo '<td>' . $data['nom'] . '</td>';
+            } else {
+                echo '<td><s>' . $data['nom'] . '</s></td>';
+            }
+            //echo '<td>' . $data['check'] . '</td>';
+            echo '&nbsp;';
+            $url = '?position=' . $position; ?>
+            <td>
+                <form action="save.php<?= $url ?>" method="POST">
+                    <input type="text" name="nom" value="<?= $data['nom'] ?? '' ?>">
+                    <?php if ($data['check'] == 0) {
+                        //on définit la variable $checked
+                         $checked = 'unchecked';
+                    } else {
+                         $checked = 'checked';
+                    } ?>
+                    <input type="checkbox" name="check" <?=$checked ?> value="1">
+                    <input type="submit" value="Complete" >
+                    
+
+            </td>
+            </form>
+    <?php
+            //Bouton modifier
             echo '<td><a href="index.php?position=' . $position . '"> <input type="submit" value="Modifier"> </a></td>';
             echo '&nbsp;';
 
-
-        //Bouton supprimer 
-        echo '<td><a onclick="return confirm(\'Voulez vous vraiment supprimer cet élément?\')" href="supprimer.php?position=' . $position . '"> <input type="submit" value="Supprimer" class="btn btn-danger"> </a></td>';
-        echo '</tr>';
+            //Bouton supprimer 
+            echo '<td><a onclick="return confirm(\'Voulez vous vraiment supprimer cet élément?\')" href="supprimer.php?position=' . $position . '"> <input type="submit" value="Supprimer"> </a></td>';
+            echo '</tr>';
+        }
+        echo '</table>';
     }
-    echo '</table>';
     ?>
-
 </body>
 
 </html>
