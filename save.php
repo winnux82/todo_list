d7b1ae7f931b88c41a2d175448f529f7ae08d6fe
WnX <vandermeulen.christophe@gmail.com>
2022-04-06 13:46:58 +0200

    :tada: save.php :tada:

--- save.php
+++ save.php
@@ -9,6 +9,28 @@ if (isset($_GET['position'])) {
     $position = $_GET['position'];
 }
 
-$data=$_POST;
+$data = $_POST;
 
-print_r($data);
+if (isset($data['nom']) === false || empty($data['nom'])) {
+    echo 'Le nom est obligatoire';
+    echo "<a href='index.php'>Retour au formulaire</a>";
+    exit;
+
+}
+
+//  Création du tableau donnees s'il n'existe pas
+if (isset($_SESSION['donnees']) === false) {
+    $_SESSION['donnees'] = [];}
+
+//ajout d'une donnees à notre tableau donnee
+//Si la position n'est pas nulle, on modifie la donnee position avec la variable $donnees qui est rempli de $_POST
+if ($position !== null) {
+    $_SESSION['donnees'][$position] = $data;
+//Si position est nulle, on ajoute la donnee
+} else {
+    $_SESSION['donnees'][] = $data;
+}
+
+//on redirige vers le fichier liste.php
+header('location: index.php');
+exit;
